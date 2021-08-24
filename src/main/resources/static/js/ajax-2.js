e7cdb0abfc64bef1ab5f6a899d38e2ad51c733ca
tosumaso <wakuwaku5712@gmail.com>
2021-08-24 16:55:07 +0900

    FileReaderまとめ

--- src/main/resources/static/js/ajax-2.js
+++ src/main/resources/static/js/ajax-2.js
@@ -1,29 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
 	
-	const zone = document.querySelector("#drop-zone");
+	const zone = document.querySelector("#drop-zone"); //ドラッグ先の要素
 
-	zone.addEventListener("dragover", (e) => {
-		e.stopPropagation();
+	zone.addEventListener("dragover", (e) => { //ドラッグ中のコンテンツをドラッグ先の要素に掲げている状態
+		e.stopPropagation(); //イベントの伝搬を止める(親要素にdragoverイベントが登録されていないので書かなくても良い)
 		e.preventDefault();
-		zone.style.background = "#ff3399";
+		zone.style.background = "#ff3399"; //id#drop-zoneのstyle属性のbackgroundプロパティに色を代入
 	})
-	zone.addEventListener("dragleave", (e) => {
+	zone.addEventListener("dragleave", () => { //ドラッグ中のコンテンツがドラッグ先の要素から外れた場合
 		zone.style.background = "#ffffff";
 	})
-	zone.addEventListener("drop", (e) => {
+	zone.addEventListener("drop", (e) => { //ドラッグ中のコンテンツがドラッグ先の要素にペーストされた状態
 		e.stopPropagation();
 		e.preventDefault();
 
 		zone.style.background = "#ffffff";
 
-		const files = e.dataTransfer.files;
+		const files = e.dataTransfer.files; //ドラッグしているファイルについてFileListが管理
 
-		const fr = new FileReader()
+		const fr = new FileReader() // ファイルの読み込みに関するオブジェクト
 		for (let i = 0; i < files.length; i++) {
-			fr.readAsDataURL(files[i]);
-			fr.addEventListener("load", () => {
+			fr.readAsDataURL(files[i]); //FileListの配列に格納されているFileを１つずつ読み込みURIを取得
+			fr.addEventListener("load", () => { //　ファイルリーダーがアップロードした画像を全て読み込んだ場合
 				const img = document.createElement('img'); 
-				img.setAttribute('src', fr.result);
+				img.setAttribute('src', fr.result); //FileReader.resultで読み込んだ画像の結果を取得する
 				
 				const preview = document.getElementById("preview");
 				preview.appendChild(img);
